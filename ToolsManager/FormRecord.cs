6e88a546915f2f071dc8eb2137d0e0b03242169c
QclxyYYQ <324498020@qq.com>
2016-11-19 15:07:04 +0800

    完成我的领用表

--- ToolsManager/FormRecord.cs
+++ ToolsManager/FormRecord.cs
@@ -13,12 +13,27 @@ namespace ToolsManager
 {
     public partial class FormRecord : Form
     {
+
         private string lastTable;
-        private int curPage, maxPageNum;
+        /// <summary>
+        /// 当前页码
+        /// </summary>
+        private int curPage;
+        /// <summary>
+        /// 总页数
+        /// </summary>
+        private int maxPageNum;
+        /// <summary>
+        /// 标记位，防止comboBox1的comboBox1_SelectedIndexChanged误触发
+        /// </summary>
         private bool isInit;
+        private DataGridViewCellStyle color_danger = new DataGridViewCellStyle();
+
         public FormRecord()
         {
             InitializeComponent();
+            color_danger.BackColor = Color.Red;
+            color_danger.ForeColor = Color.White;
         }
 
         private void FormRecord_Load(object sender, EventArgs e)
@@ -32,15 +47,17 @@ namespace ToolsManager
         }
         async public Task<bool> TableReceiveList(int cur)
         {
-            if (cur <= 0)
-                cur = 1;
             if (await Server.GetReceiveList(Global.LoginInfo.user_id, Global.LoginInfo.user_code, cur, Global.PageNum))
             {
                 lb_cur.Text = "第" + Global.ReceiveList.num.page + "页";
-                var sum = Global.ReceiveList.num.list_num / Global.PageNum;
-                maxPageNum = sum == 0 ? 1 : sum;
+                maxPageNum = Global.ReceiveList.num.list_num / Global.PageNum + 1;
                 lb_sum.Text = "共" + maxPageNum + "页";
                 curPage = cur;
+
+                isInit = true;
+                Global.AddComboxNum(comboBox1, maxPageNum);
+                comboBox1.SelectedIndex = Global.ReceiveList.num.page - 1;
+                isInit = false;
 
                 //此处不应该改全局变量里的List来改变显示内容
                 //for (int i = 0; i < Global.ReceiveList.list.Count; i++)
@@ -59,43 +76,153 @@ namespace ToolsManager
                 //}
 
                 dataGridView1.DataSource = Global.ReceiveList.list;
-                
+
                 for (int i = 0; i < dataGridView1.Rows.Count; i++)
                 {
                     var t = dataGridView1.Rows[i].Cells;
-                    if (t[10].Value as string == "1")
+                    var type = t[10].Value as string;
+                    if (type == "1")//type
                     {
-                        var c = new DataGridViewCellStyle(t[10].Style);
-                        c.BackColor = Color.Red;
-                        c.ForeColor = Color.White;
-                        t[10].Style = c;
+
+                        t[10].Style = color_danger;
                         //对value的修改会影响绑定的List，务必注意
                         t[9].Value = t[7].Value = t[14].Value = t[5].Value = "";
                         t[10].Value = "未归还";
-                        
+
+
                     }
-                    else
+                    else if (type == "2")
                     {
                         //对value的修改会影响绑定的List，务必注意
                         t[10].Value = "已归还";
+                        if (t[9].Value as string == "1")
+                        {
+                            t[9].Value = "完好";
+                        }
+                        else
+                        {
+                            t[9].Value = "否";
+                            t[9].Style = color_danger;
+                        }
+                    }
+                    if (t[8].Value as string == "1")//领用时
+                    {
+                        t[8].Value = "完好";
+                    }
+                    else
+                    {
+                        t[8].Value = "否";
+                        t[9].Style = color_danger;
+
                     }
                 }
-                dataGridView1.Columns[0].HeaderText = "记录id";
-                dataGridView1.Columns[1].HeaderText = "工具id";
-                dataGridView1.Columns[2].HeaderText = "站点id";
-                dataGridView1.Columns[3].HeaderText = "工作票号id";
-                dataGridView1.Columns[4].HeaderText = "领用人id";
-                dataGridView1.Columns[5].HeaderText = "归还人id";
-                dataGridView1.Columns[6].HeaderText = "领用时间";
-                dataGridView1.Columns[7].HeaderText = "归还时间";
-                dataGridView1.Columns[8].HeaderText = "领用时是否完好";
-                dataGridView1.Columns[9].HeaderText = "归还时是否完好";
-                dataGridView1.Columns[10].HeaderText = "是否归还";
-                dataGridView1.Columns[11].HeaderText = "工具名称";
-                dataGridView1.Columns[12].HeaderText = "工具编号";
-                dataGridView1.Columns[13].HeaderText = "领用人名称";
-                dataGridView1.Columns[14].HeaderText = "归还人名称";
-                dataGridView1.Columns[15].HeaderText = "工作票号";
+                if (dataGridView1.ColumnCount > 0)
+                {
+                    dataGridView1.Columns[0].HeaderText = "记录id";
+                    dataGridView1.Columns[1].HeaderText = "工具id";
+                    dataGridView1.Columns[2].HeaderText = "站点id";
+                    dataGridView1.Columns[3].HeaderText = "工作票号id";
+                    dataGridView1.Columns[4].HeaderText = "领用人id";
+                    dataGridView1.Columns[5].HeaderText = "归还人id";
+                    dataGridView1.Columns[6].HeaderText = "领用时间";
+                    dataGridView1.Columns[7].HeaderText = "归还时间";
+                    dataGridView1.Columns[8].HeaderText = "领用时是否完好";
+                    dataGridView1.Columns[9].HeaderText = "归还时是否完好";
+                    dataGridView1.Columns[10].HeaderText = "是否归还";
+                    dataGridView1.Columns[11].HeaderText = "工具名称";
+                    dataGridView1.Columns[12].HeaderText = "工具编号";
+                    dataGridView1.Columns[13].HeaderText = "领用人名称";
+                    dataGridView1.Columns[14].HeaderText = "归还人名称";
+                    dataGridView1.Columns[15].HeaderText = "工作票号";
+                }
+
+                for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                {
+                    //全部列不可修改
+                    dataGridView1.Columns[i].ReadOnly = true;
+                    //拉伸列宽来填满表格
+                    dataGridView1.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+
+                }
+
+
+            }
+            return true;
+        }
+        async public Task<bool> UserReceiveList(int cur)
+        {
+            if (await Server.GetUserReceiveList(Global.LoginInfo.user_id, Global.LoginInfo.user_code, cur, Global.PageNum))
+            {
+                lb_cur.Text = "第" + Global.UserReceiveList.num.page + "页";
+                maxPageNum = Global.UserReceiveList.num.list_num / Global.PageNum + 1;
+                lb_sum.Text = "共" + maxPageNum + "页";
+                curPage = cur;
+
+                isInit = true;
+                Global.AddComboxNum(comboBox1, maxPageNum);
+                comboBox1.SelectedIndex = Global.UserReceiveList.num.page - 1;
+                isInit = false;
+
+                dataGridView1.DataSource = Global.UserReceiveList.list;
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    var t = dataGridView1.Rows[i].Cells;
+                    var type = t[10].Value as string;
+                    if (type == "1")//type
+                    {
+
+                        t[10].Style = color_danger;
+                        //对value的修改会影响绑定的List，务必注意
+                        t[9].Value = t[7].Value = t[14].Value = t[5].Value = "";
+                        t[10].Value = "未归还";
+
+                    }
+                    else if (type == "2")
+                    {
+                        //对value的修改会影响绑定的List，务必注意
+                        t[10].Value = "已归还";
+                        if (t[9].Value as string == "1")
+                        {
+                            t[9].Value = "完好";
+                        }
+                        else
+                        {
+                            t[9].Value = "否";
+                            t[9].Style = color_danger;
+                        }
+                    }
+                    if (t[8].Value as string == "1")//领用时
+                    {
+                        t[8].Value = "完好";
+                    }
+                    else
+                    {
+                        t[8].Value = "否";
+                        t[9].Style = color_danger;
+
+                    }
+                }
+                if (dataGridView1.ColumnCount > 0)
+                {
+                    dataGridView1.Columns[0].HeaderText = "记录id";
+                    dataGridView1.Columns[1].HeaderText = "工具id";
+                    dataGridView1.Columns[2].HeaderText = "站点id";
+                    dataGridView1.Columns[3].HeaderText = "工作票号id";
+                    dataGridView1.Columns[4].HeaderText = "领用人id";
+                    dataGridView1.Columns[5].HeaderText = "归还人id";
+                    dataGridView1.Columns[6].HeaderText = "领用时间";
+                    dataGridView1.Columns[7].HeaderText = "归还时间";
+                    dataGridView1.Columns[8].HeaderText = "领用时是否完好";
+                    dataGridView1.Columns[9].HeaderText = "归还时是否完好";
+                    dataGridView1.Columns[10].HeaderText = "是否归还";
+                    dataGridView1.Columns[11].HeaderText = "工具名称";
+                    dataGridView1.Columns[12].HeaderText = "工具编号";
+                    dataGridView1.Columns[13].HeaderText = "领用人名称";
+                    dataGridView1.Columns[14].HeaderText = "归还人名称";
+                    dataGridView1.Columns[15].HeaderText = "工作票号";
+                }
+
 
                 for (int i = 0; i < dataGridView1.Columns.Count; i++)
                 {
@@ -116,21 +243,14 @@ namespace ToolsManager
             {
                 case "领还明细":
                     lastTable = "领还明细";
-                    //comboBox1.SelectedIndex = 0;
                     await TableReceiveList(1);
-                    isInit = true;
-                    if (comboBox1.Items.Count != maxPageNum)
-                    {
-                        Global.AddComboxNum(comboBox1, maxPageNum);
-                        comboBox1.SelectedIndex = 0;
-                    }
-                    isInit = false;
                     break;
                 case "现存库存":
                     lastTable = "现存库存";
                     break;
                 case "我的领用":
                     lastTable = "我的领用";
+                    await UserReceiveList(1);
                     break;
                 case "单号事件记录表":
                     lastTable = "单号事件记录表";
@@ -149,13 +269,15 @@ namespace ToolsManager
             {
                 case "领还明细":
                     lastTable = "领还明细";
-                    await TableReceiveList(curPage - 1);
+                    await TableReceiveList(curPage - 1 < 1 ? 1 : curPage - 1);
                     break;
                 case "现存库存":
                     lastTable = "现存库存";
                     break;
                 case "我的领用":
                     lastTable = "我的领用";
+                    await UserReceiveList(curPage - 1 < 1 ? 1 : curPage - 1);
+
                     break;
                 case "单号事件记录表":
                     lastTable = "单号事件记录表";
@@ -176,6 +298,8 @@ namespace ToolsManager
                     break;
                 case "我的领用":
                     lastTable = "我的领用";
+                    await UserReceiveList(curPage + 1 > maxPageNum ? maxPageNum : curPage + 1);
+
                     break;
                 case "单号事件记录表":
                     lastTable = "单号事件记录表";
@@ -196,6 +320,7 @@ namespace ToolsManager
                     break;
                 case "我的领用":
                     lastTable = "我的领用";
+                    await UserReceiveList(1);
                     break;
                 case "单号事件记录表":
                     lastTable = "单号事件记录表";
@@ -216,6 +341,7 @@ namespace ToolsManager
                     break;
                 case "我的领用":
                     lastTable = "我的领用";
+                    await UserReceiveList(comboBox1.Items.Count);
                     break;
                 case "单号事件记录表":
                     lastTable = "单号事件记录表";
@@ -238,17 +364,13 @@ namespace ToolsManager
                         break;
                     case "我的领用":
                         lastTable = "我的领用";
+                        await UserReceiveList(comboBox1.SelectedIndex + 1);
                         break;
                     case "单号事件记录表":
                         lastTable = "单号事件记录表";
                         break;
                 }
             }
-        }
-
-        private void ll_Goto_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-
         }
     }
 }
